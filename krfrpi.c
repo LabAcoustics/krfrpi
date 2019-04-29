47610ccb78761dadcae043bc4f802d7cb5bb87eb
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-04-30 01:04:13 +0200

    Detabify

--- krfrpi.c
+++ krfrpi.c
@@ -48,7 +48,7 @@ static DECLARE_WAIT_QUEUE_HEAD(rx433_wait);
 
 /* Define GPIOs for RX signal */
 static struct gpio signals[] = {
-		{ GPIO_FOR_RX_SIGNAL, GPIOF_IN, "RX Signal" },	// Rx signal
+    { GPIO_FOR_RX_SIGNAL, GPIOF_IN, "RX Signal" },	// Rx signal
 };
 
 /* Later on, the assigned IRQ numbers for the buttons are stored here */
@@ -59,29 +59,29 @@ static int rx_irqs[] = { -1 };
  */
 static irqreturn_t rx_isr(int irq, void *data)
 {
-   	struct timespec current_time;
+    struct timespec current_time;
     struct timespec delta;
-   	unsigned long long ns;
+    unsigned long long ns;
 
-   	getnstimeofday(&current_time);
-	delta = timespec_sub(current_time, lastIrq_time);
-	ns = ((long long)delta.tv_sec * 1000000000)+(delta.tv_nsec); 
-	lastDelta[pWrite] = ns;
-   	getnstimeofday(&lastIrq_time);
+    getnstimeofday(&current_time);
+    delta = timespec_sub(current_time, lastIrq_time);
+    ns = ((long long)delta.tv_sec * 1000000000)+(delta.tv_nsec); 
+    lastDelta[pWrite] = ns;
+    getnstimeofday(&lastIrq_time);
 
-	pWrite = ( pWrite + 1 )  & (BUFFER_SZ-1);
-	if (pWrite == pRead) {
-		// overflow
-		pRead = ( pRead + 1 ) & (BUFFER_SZ-1);
-		if ( wasOverflow == 0 ) {
-	       printk(KERN_ERR "RFRPI - Buffer Overflow - IRQ will be missed");
-	       wasOverflow = 1;
-	    }
-	} else {
-		wasOverflow = 0;
-	}
+    pWrite = ( pWrite + 1 )  & (BUFFER_SZ-1);
+    if (pWrite == pRead) {
+        // overflow
+        pRead = ( pRead + 1 ) & (BUFFER_SZ-1);
+        if ( wasOverflow == 0 ) {
+            printk(KERN_ERR "RFRPI - Buffer Overflow - IRQ will be missed");
+            wasOverflow = 1;
+        }
+    } else {
+        wasOverflow = 0;
+    }
     wake_up_interruptible(&rx433_wait);
-	return IRQ_HANDLED;
+    return IRQ_HANDLED;
 }
 
 
@@ -96,40 +96,40 @@ static int rx433_release(struct inode *inode, struct file *file)
 }
 
 static ssize_t rx433_write(struct file *file, const char __user *buf,
-                size_t count, loff_t *pos)
+        size_t count, loff_t *pos)
 {
-	return -EINVAL;
+    return -EINVAL;
 }
 
 static ssize_t rx433_read(struct file *file, char __user *buf,
-                size_t count, loff_t *pos)
+        size_t count, loff_t *pos)
 {
-	// returns one of the line with the time between two IRQs
-	// return 0 : end of reading
-	// return >0 : size
-	// return -EFAULT : error
-	char tmp[256];
-	int _count;
-	int _error_count;
+    // returns one of the line with the time between two IRQs
+    // return 0 : end of reading
+    // return >0 : size
+    // return -EFAULT : error
+    char tmp[256];
+    int _count;
+    int _error_count;
 
-	_count = 0;
-	if ( pRead != pWrite ) {
-		sprintf(tmp,"%ld\n",lastDelta[pRead]);
-  	    _count = strlen(tmp);
+    _count = 0;
+    if ( pRead != pWrite ) {
+        sprintf(tmp,"%ld\n",lastDelta[pRead]);
+        _count = strlen(tmp);
         _error_count = copy_to_user(buf,tmp,_count+1);
         if ( _error_count != 0 ) {
-        	printk(KERN_ERR "RFRPI - Error writing to char device");
+            printk(KERN_ERR "RFRPI - Error writing to char device");
             return -EFAULT;
         }
-		pRead = (pRead + 1) & (BUFFER_SZ-1);
-	}
-	return _count;
+        pRead = (pRead + 1) & (BUFFER_SZ-1);
+    }
+    return _count;
 }
 
 static unsigned int rx433_poll(struct file *file, poll_table *wait)
 {
     poll_wait(file, &rx433_wait, wait);
-	if ( pRead != pWrite ) {
+    if ( pRead != pWrite ) {
         return POLLIN | POLLRDNORM;
     }
     return 0;
@@ -157,49 +157,49 @@ static struct miscdevice rx433_misc_device = {
  */
 static int __init rfrpi_init(void)
 {
-	int ret = 0;
-	printk(KERN_INFO "%s\n", __func__);
+    int ret = 0;
+    printk(KERN_INFO "%s\n", __func__);
 
-	// INITIALIZE IRQ TIME AND Queue Management
-	getnstimeofday(&lastIrq_time);
-	pRead = 0;
-	pWrite = 0;
-	wasOverflow = 0;
+    // INITIALIZE IRQ TIME AND Queue Management
+    getnstimeofday(&lastIrq_time);
+    pRead = 0;
+    pWrite = 0;
+    wasOverflow = 0;
 
-	// register GPIO PIN in use
-	ret = gpio_request_array(signals, ARRAY_SIZE(signals));
+    // register GPIO PIN in use
+    ret = gpio_request_array(signals, ARRAY_SIZE(signals));
 
-	if (ret) {
-		printk(KERN_ERR "RFRPI - Unable to request GPIOs for RX Signals: %d\n", ret);
-		goto fail2;
-	}
-	
-	// Register IRQ for this GPIO
-	ret = gpio_to_irq(signals[0].gpio);
-	if(ret < 0) {
-		printk(KERN_ERR "RFRPI - Unable to request IRQ: %d\n", ret);
-		goto fail2;
-	}
-	rx_irqs[0] = ret;
-	printk(KERN_INFO "RFRPI - Successfully requested RX IRQ # %d\n", rx_irqs[0]);
-	ret = request_irq(rx_irqs[0], rx_isr, IRQF_TRIGGER_RISING, "rfrpi#rx", NULL);
-	if(ret) {
-		printk(KERN_ERR "RFRPI - Unable to request IRQ: %d\n", ret);
-		goto fail3;
-	}
+    if (ret) {
+        printk(KERN_ERR "RFRPI - Unable to request GPIOs for RX Signals: %d\n", ret);
+        goto fail2;
+    }
 
-	// Register a character device for communication with user space
+    // Register IRQ for this GPIO
+    ret = gpio_to_irq(signals[0].gpio);
+    if(ret < 0) {
+        printk(KERN_ERR "RFRPI - Unable to request IRQ: %d\n", ret);
+        goto fail2;
+    }
+    rx_irqs[0] = ret;
+    printk(KERN_INFO "RFRPI - Successfully requested RX IRQ # %d\n", rx_irqs[0]);
+    ret = request_irq(rx_irqs[0], rx_isr, IRQF_TRIGGER_RISING, "rfrpi#rx", NULL);
+    if(ret) {
+        printk(KERN_ERR "RFRPI - Unable to request IRQ: %d\n", ret);
+        goto fail3;
+    }
+
+    // Register a character device for communication with user space
     misc_register(&rx433_misc_device);
 
-	return 0;
+    return 0;
 
-	// cleanup what has been setup so far
+    // cleanup what has been setup so far
 fail3:
-	free_irq(rx_irqs[0], NULL);
+    free_irq(rx_irqs[0], NULL);
 
 fail2: 
-	gpio_free_array(signals, ARRAY_SIZE(signals));
-	return ret;	
+    gpio_free_array(signals, ARRAY_SIZE(signals));
+    return ret;	
 }
 
 /**
@@ -207,15 +207,15 @@ fail2:
  */
 static void __exit rfrpi_exit(void)
 {
-	printk(KERN_INFO "%s\n", __func__);
+    printk(KERN_INFO "%s\n", __func__);
 
     misc_deregister(&rx433_misc_device);
 
-	// free irqs
-	free_irq(rx_irqs[0], NULL);	
-	
-	// unregister
-	gpio_free_array(signals, ARRAY_SIZE(signals));
+    // free irqs
+    free_irq(rx_irqs[0], NULL);	
+
+    // unregister
+    gpio_free_array(signals, ARRAY_SIZE(signals));
 }
 
 MODULE_LICENSE("GPL");
